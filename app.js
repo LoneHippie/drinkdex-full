d7080aa0981e12b56d6e6fcdf863213c53c57807
LoneHippie <lonehippie.music@gmail.com>
2021-04-16 14:25:02 +0300

    prep for push to heroku

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const morgan = require('morgan');
 const cors = require('cors');
-const path = require('path');
 
 const cookieParser = require('cookie-parser');
 
